8956919e9804a427dbabc97fa051bf734a7308bd
Emmanex01 <nwanosiketochukwu99@gmail.com>
2025-01-12 23:32:12 +0100

    Updating UI Responsiveness

--- vite-project/src/components/ui/NavBar.tsx
+++ vite-project/src/components/ui/NavBar.tsx
@@ -7,16 +7,16 @@ import { FaRegUser } from "react-icons/fa";
 
 const NavBar = () => {
     return (
-        <div className="grid grid-cols-5 text-sm sticky bottom-0 bg-white py-2 md:bg-slate-600 md:sticky md:top-0 md:self-start md:h-lvh md:grid-cols-1 md:w-48" >
-            <div className="flex flex-col justify-center items-center md:flex-row">
+        <div className="grid grid-cols-5 text-sm sticky bottom-0 bg-white py-2 md:bg-slate-600 md:sticky md:top-0 md:self-start md:h-lvh md:flex md:flex-col md:w-48 md:items-start md:gap-7" >
+            <div className="flex flex-col justify-center items-center md:flex-row ">
                 <FaHome className="h-8 w-12 text-green-500"/>
                 <h2>Home</h2>
             </div>
-            <div className="flex flex-col justify-center items-center md:flex-row">
+            <div className="flex flex-col justify-center items-center md:flex-row ">
                 <FaArrowRightArrowLeft className="h-8 w-12"/>
                 <h2>Transaction</h2>
             </div>
-            <div className="flex flex-col justify-center items-center md:flex-row">
+            <div className="flex flex-col justify-center items-center md:flex-row ">
                 <FaCirclePlus className="h-8 w-12"/>
                 <h2>Home</h2>
             </div>
